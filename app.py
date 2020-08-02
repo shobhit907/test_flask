15192422ba99c122f27ad59f189f6cede5dfeda2
shobhit <shobhitgupta907@gmail.com>
2020-08-03 00:51:53 +0530

    Predict digits

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 import os
 from PIL import Image
+import makePrediction
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-UPLOAD_FOLDER='../tmp/'
-# UPLOAD_FOLDER='./uploads/'
+# UPLOAD_FOLDER='../tmp/'
+UPLOAD_FOLDER='./uploads/'
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
 
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -32,7 +36,7 @@ def image_detail():
         filename=file.filename
         if(secure_filename(filename) and allowed_file(filename)):
             path=os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            # print(path)
+            print(path)
             file.save(path)
             img=Image.open(path)
             return render_template('imageDetail.html',image_detail=str(img.size))
@@ -43,6 +47,19 @@ def image_detail():
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/predict/',methods=["POST"])
+def predict_digit():
+    if(request.method=="POST"):
+        file=request.files['image_digit']
+        filename=file.filename
+        if(secure_filename(filename) and allowed_file(filename)):
+            path=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            print(path)
+            file.save(path)
+            return makePrediction.predict(path)
+        else:
+            return "Invalid"
 
 if __name__=='__main__':
     app.run(debug=True,host="127.0.0.1",port=8000)
